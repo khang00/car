edeb11c0c4b770bb2e15111442a0f3e3430fec20
khang00 <dlqkhang2@gmail.com>
2020-07-15 09:44:59 +0700

    init

---
+++ src/main/kotlin/khang/revive/car/CarApplication.kt
@@ -0,0 +1,11 @@
+package khang.revive.car
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CarApplication
+
+fun main(args: Array<String>) {
+	runApplication<CarApplication>(*args)
+}
---
+++ src/test/kotlin/khang/revive/car/CarApplicationTests.kt
@@ -0,0 +1,13 @@
+package khang.revive.car
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CarApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
