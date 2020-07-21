70a2c37864cccfc48fc49a2f3afa62bef00ebf94
khang00 <dlqkhang2@gmail.com>
2020-07-21 14:54:52 +0700

    update

--- src/main/kotlin/khang/revive/car/model/Employee.kt
+++ src/main/kotlin/khang/revive/car/model/Employee.kt
@@ -6,8 +6,8 @@ import org.springframework.data.mongodb.core.mapping.Document
 
 @Document
 data class Employee(@Id val id: String = ObjectId.get().toHexString(),
-                    val role: EmployeeRole,
-                    val name: String,
+                    val role: EmployeeRole = EmployeeRole.SALE,
+                    val name: String = "",
                     val account: String,
                     val password: String = "",
                     val imageUrls: List<String> = listOf("https://www.shareicon.net/data/128x128/2016/05/24/770137_man_512x512.png"))
