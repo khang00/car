d9d0527361d9d57db9dd870f8684e29ba39db459
khang00 <dlqkhang2@gmail.com>
2020-07-21 13:18:01 +0700

    add CORS

--- src/main/kotlin/khang/revive/car/CarApplication.kt
+++ src/main/kotlin/khang/revive/car/CarApplication.kt
@@ -2,10 +2,20 @@ package khang.revive.car
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
+@EnableWebMvc
 @SpringBootApplication
-class CarApplication
+class CarApplication : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+    }
+}
 
 fun main(args: Array<String>) {
-	runApplication<CarApplication>(*args)
+    runApplication<CarApplication>(*args)
 }
